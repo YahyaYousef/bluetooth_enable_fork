e8709bf68da590359a51a23ae3ff5d1ee65a1872
Yahya Yousef <55236141+YahyaYousef@users.noreply.github.com>
2025-07-23 16:21:31 +0300

    fix

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<bluetooth_enable_fork/BluetoothEnablePlugin.h>)
+#import <bluetooth_enable_fork/BluetoothEnablePlugin.h>
+#else
+@import bluetooth_enable_fork;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [BluetoothEnablePlugin registerWithRegistrar:[registry registrarForPlugin:@"BluetoothEnablePlugin"]];
+}
+
+@end
